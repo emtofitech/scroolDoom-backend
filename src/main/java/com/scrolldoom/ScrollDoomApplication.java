d2abe5cf7b4c1a34821c494f0776111522ab7990
Martins-O <jojololamartins686@gmail.com>
2026-05-22 09:05:00 +0100

    feat: add main application entry point

---
+++ src/main/java/com/scrolldoom/ScrollDoomApplication.java
@@ -0,0 +1,12 @@
+package com.scrolldoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrollDoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrollDoomApplication.class, args);
+    }
+}
